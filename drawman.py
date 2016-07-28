75810ee48c368f14b071994e5846686415b4b090
mlir1986 <alyona1986>
2016-07-28 12:13:03 +0300

    Сделано масштабирование /Чертежника

--- drawman.py
+++ drawman.py
@@ -1,12 +1,13 @@
 from turtle import Turtle
 
 def init_drawman():
-    global t, x_current, y_current
+    global t, x_current, y_current, dm_scale
     t=Turtle()
     t.penup()
     x_current=0
     y_current=0
     t.goto(x_current, y_current)
+    dm_scale = 10
 
 
 def test_drawman():
@@ -21,6 +22,7 @@ def test_drawman():
     pen_up()
     to_point(0,0)
 
+
 def pen_down():
     t.pendown()
 
@@ -28,16 +30,13 @@ def pen_up():
     t.penup()
 
 def on_vector(dx,dy):
-    global x_current, y_current
-    x_current+=dx
-    y_current+=dy
-    t.goto(x_current, y_current)
+    to_point(x_current + dx, y_current + dy)
 
-def to_point(a,b):
+def to_point(x,y):
     global x_current, y_current
-    x_current = a
-    y_current = b
-    t.goto(a,b)
+    x_current = x
+    y_current = y
+    t.goto(dm_scale*x,dm_scale*y)
 
 init_drawman()
 if __name__ == '__main__':
